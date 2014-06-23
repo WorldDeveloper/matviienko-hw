32bf6c7d76cabfca242ca7337883c08f979fa16c
matviienkos@gmail.com <matviienkos@gmail.com@7c4d67bf-a6d6-bd9e-cbfa-6ceb9c08b31b>
2014-06-23 18:18:54 +0000

    cpp26-1 is added

---
+++ Cpp/cpp26/26-1/Bandit.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+
+class Rell
+{
+	int mSize;
+	int mRoll[10];
+public:
+	Rell(const int size = 10) :mSize(size)
+	{
+		for (int i = 0; i < size; ++i)
+		{
+			mRoll[i] = i;
+		}
+	}
+
+	int Pop()
+	{
+		int tmp = mRoll[0];
+		for (int i = 1; i < mSize; ++i)
+		{
+			mRoll[i - 1] = mRoll[i];
+		}
+		mRoll[mSize-1] = tmp;
+		
+		return tmp;
+	}
+	int Begin() const { return mRoll[0]; }
+};
+
+
