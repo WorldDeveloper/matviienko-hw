2b0a6b8908575fe92757939b4753817d1f625e4c
matviienkos@gmail.com <matviienkos@gmail.com@7c4d67bf-a6d6-bd9e-cbfa-6ceb9c08b31b>
2014-07-04 22:11:47 +0000

    cpp25-3 is modified

--- Cpp/cpp25/25-3/25-3.cpp
+++ Cpp/cpp25/25-3/25-3.cpp
@@ -6,28 +6,132 @@
 #include <iostream>
 #include <typeinfo>
 #include <cstdarg>
+#include <iomanip>
 using namespace std;
 
-template <typename T>
-void AnyNumParam(const int count, T arg ...)
-{
-	va_list pArg;
-	va_start(pArg, count);
 
-	cout << "\n\nThere are " << count << " \"";
-	cout << typeid(va_arg(pArg, T)).name() << "\" parametrs in the function:\n";
-
-	for (int i = 0; i < count; ++i)
-	{
-		cout << va_arg(pArg, T) << " ";
-	}
-	va_end(pArg);
-}
+void PrintFormat(const char* output, ...);
 
 void main()
 {
-	AnyNumParam(3, "aaa", "bbb", "ccc");
-	AnyNumParam(4, 'a', 'b', 'c', 'd');
-	AnyNumParam(5, 1, 2, 3, 4, 5);
-	AnyNumParam(3, 1.1, 'a', 1);//don't work properly
+	PrintFormat("%10.5s %c%c     %s\n\n", "Hello!!!!!!!!!!!!", 't', 'o', "all!");
+	printf("%10.5s %c%c     %s\n\n", "Hello!!!!!!!!!!!!", 't', 'o',"all!");
+
+	PrintFormat("|%-10.1f|\n\n", 123.123456);
+	printf("|%-10.1f|\n\n", 123.123456);
+
+	PrintFormat("%s %c %d%%\n\n", "rate", '=', 15);
+	printf("%s %c %d%%\n\n", "rate", '=', 15);
+}
+
+
+void PrintFormat(const char* output, ...)
+{
+	va_list pArg;
+	va_start(pArg, output);
+
+	const char* curSymbol = output;
+	while (*curSymbol != '\0')
+	{
+		if (*curSymbol == '%')
+		{
+			++curSymbol;
+			bool leftAlign = false;
+			if (*curSymbol != '\0'&& *curSymbol == '-')
+			{
+				cout << left;
+				leftAlign = true;
+				++curSymbol;
+			}
+			else
+			{
+				cout << right;
+			}
+
+			char fill = ' ';
+			if (*curSymbol != '\0'&& *curSymbol == '0')
+			{
+				fill = '0';
+				++curSymbol;
+			}
+
+			char fillLen[100];
+			char* pLen = fillLen;
+			while (*curSymbol > 47 && *curSymbol < 58)
+			{
+				*pLen++ = *curSymbol++;
+			}
+			*pLen = '\0';
+			int fillLength = atoi(fillLen);
+
+			if (fillLength)
+			{
+				cout << setfill(fill) << setw(fillLength);
+			}
+
+			int precision;
+			char precisionChar[100];
+			char* pPrecision = precisionChar;
+			if (*curSymbol != '\0'&& *curSymbol == '.')
+			{
+				curSymbol++;
+				while (*curSymbol > 47 && *curSymbol < 58)
+				{
+					*pPrecision++ = *curSymbol++;
+				}
+				*pPrecision = '\0';
+				precision = atoi(precisionChar);
+			}
+			else
+			{
+				precision = -1;
+			}
+
+			switch (*curSymbol)
+			{
+			case 'd':
+			case'i':
+				cout << va_arg(pArg, int);
+				break;
+			case'u':
+				cout << va_arg(pArg, unsigned int);
+				break;
+			case'f':
+				if (precision >= 0)
+				{
+					cout << fixed << setprecision(precision);
+					if (leftAlign) cout << setfill(' ');
+				}
+				cout << va_arg(pArg, double);
+				break;
+			case'c':
+				cout << va_arg(pArg, char);
+				break;
+			case's':
+				char tmp[100];
+				strcpy(tmp, va_arg(pArg, char*));
+
+				if (precision >= 0 && precision < strlen(tmp))
+				{
+					tmp[precision] = '\0';
+				}
+				cout << tmp;
+				break;
+			case '%':
+				cout << '%';
+				break;
+			default:
+				cout << " ";
+				break;
+			}
+		}
+		else
+		{
+			cout << *curSymbol;
+		}
+
+		curSymbol++;
+	}
+
+	va_end(pArg);
 }
