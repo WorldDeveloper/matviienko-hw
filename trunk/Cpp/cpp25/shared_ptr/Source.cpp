9f1f554138da95c0622895e81040d26d7a63a8db
matviienkos@gmail.com <matviienkos@gmail.com@7c4d67bf-a6d6-bd9e-cbfa-6ceb9c08b31b>
2014-07-05 11:00:36 +0000

    cpp25 Sharedptr is modified

--- trunk/Cpp/cpp25/shared_ptr/Source.cpp
+++ trunk/Cpp/cpp25/shared_ptr/Source.cpp
@@ -4,36 +4,28 @@
 #include "Sharedptr.h"
 using namespace std;
 
-void f1(Sharedptr<int>& p, int count)
-{
-	if (count > 3) return;
-	*p = count;
-	cout << "\n p=" << *p << "\n # of copies: " << p.Count();
-	cout << "\n Is p unique: " << p.Unique();
-	count++;
-	f1(p, count);
-}
+template <typename T>
+void info(const Sharedptr<T>& p, char* name);
 
-void f2(Sharedptr<int> p, int count)
-{
-	if (count > 3) return;
-	*p = count;
-	cout << "\n p=" << *p << "\n # of copies: " << p.Count();
-	cout << "\n Is p unique: " << p.Unique();
-	count++;
-	f2(p, count);
-}
+void f1(Sharedptr<int>& p, int count);
+void f2(Sharedptr<int> p, int count);
 
 void main()
 {
+	cout << "Create ptr1\n";
 	Sharedptr<int> ptr1(new int);
-	*ptr1 = 1;
-	cout << "\n ptr1=" << *ptr1;
-	cout << "\n Is ptr1 unique: " << ptr1.Unique() << endl;
 
+	*ptr1 = 1;
+	cout << "ptr1=1";
+	info(ptr1, "ptr1");
+
+
+	cout << "\n\nCreate ptr2\n";
 	Sharedptr<int> ptr2(new int);
+
 	*ptr2 = 2;
-	cout << "\n ptr2=" << *ptr2;
+	cout << "ptr2=2";
+	info(ptr2, "ptr2");
 
 	cout << "\n\n ptr1==ptr2:\t" << (ptr1 == ptr2) << endl;
 	cout << " ptr1!=ptr2:\t" << (ptr1 != ptr2) << endl;
@@ -42,14 +34,45 @@ void main()
 	cout << " ptr1>ptr2:\t" << (ptr1 > ptr2) << endl;
 	cout << " ptr1<ptr2:\t" << (ptr1 < ptr2) << endl;
 
+	cout << "\nptr2=ptr1\n";
+	ptr2 = ptr1;
+	info(ptr1, "ptr1");
+	info(ptr2, "ptr2");
+
 	cout << "\n\n***Call by reference***";
 	f1(ptr1, 1);
-	cout << "\n\n***Call by reference ended***\n";
+	cout << "***Call by reference ended***\n";
 
-	cout << "\n\n*****Call by value***";
+	cout << "\n\n*****Call by value***\n";
 	f2(ptr1, 1);
-	cout << "\n\n***Call by value ended***\n";
+	cout << "***Call by value ended***\n";
 
-	cout << "\n\n ptr1=" << *ptr1;
-	cout << "\n Is ptr1 unique: " << ptr1.Unique();
+	info(ptr1, "ptr1");
+	info(ptr2, "ptr2");
+}
+
+template <typename T>
+void info(const Sharedptr<T>& p, char* name)
+{
+	cout << "\n\t " << name << "=" << *p;
+	cout << "\n\t # of copies: " << p.Count();
+	cout << "\n\t Is " << name << " unique: " << p.Unique() << endl;
+}
+
+void f1(Sharedptr<int>& p, int count)
+{
+	if (count > 3) return;
+	*p = count;
+	info(p, "p");
+	count++;
+	f1(p, count);
+}
+
+void f2(Sharedptr<int> p, int count)
+{
+	if (count > 3) return;
+	*p = count;
+	info(p, "p");
+	count++;
+	f2(p, count);
 }
