d2062d39b9f3beaa2f171ec5d90d85064c64b5ff
matviienkos@gmail.com <matviienkos@gmail.com@7c4d67bf-a6d6-bd9e-cbfa-6ceb9c08b31b>
2014-07-19 16:04:32 +0000

    cpp28-1 is added

---
+++ trunk/Cpp/cpp28/28-1/menu.h
@@ -0,0 +1,19 @@
+
+class Menu
+{
+	char mTitle[70];
+	char** mItem;
+	const int mItemsNumber;
+	const int  mItemMaxLength;
+	bool* mStatus;
+	const int mX;
+	const int mY;
+public:
+	Menu(char *name, const char size, const char length, const char* items[], const int xCoord, const int yCoord);
+	void addItems(const char* name[]);
+	void setStatus(const int activeItem);
+	void showMenu(const int activeItem = -2);
+	void gotoxy(short x, short y);
+	void line(unsigned const char c, const int length);
+	~Menu();
+};
