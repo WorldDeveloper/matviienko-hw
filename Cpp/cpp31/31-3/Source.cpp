7023342ff6617627d47a9b26470182f38a1a76c3
matviienkos@gmail.com <matviienkos@gmail.com@7c4d67bf-a6d6-bd9e-cbfa-6ceb9c08b31b>
2014-09-12 08:56:54 +0000

    cpp31-3 is added

---
+++ Cpp/cpp31/31-3/Source.cpp
@@ -0,0 +1,49 @@
+﻿/*
+Используя понятие множественного наследования, разработайте класс
+"Окружность, вписанная в квадрат".
+*/
+
+#include <iostream>
+using namespace std;
+
+class Square
+{
+	float mWidth;
+public:
+	Square(const float width) : mWidth(width){}
+	float GetSquareArea() const { return mWidth*mWidth; }
+	float GetSquarePerimeter() const { return 4 * mWidth; }
+	float GetSquareWidth() const { return mWidth; }
+};
+
+class Circle
+{
+	float mRadius;
+	const float mPi;
+public:
+	Circle(const float radius) : mRadius(radius), mPi(3.141592){}
+	float GetCircleArea() const { return mPi*mRadius*mRadius; }
+	float GetCirclePerimetr() const { return 2 * mPi*mRadius; }
+	float GetCircleRadius() const { return mRadius; }
+};
+
+class CircleInSquare :public Square, public Circle
+{
+public:
+	CircleInSquare(const float width) : Square(width), Circle(width / 2){}
+};
+
+void main()
+{
+	CircleInSquare f(10.0);
+	cout << "SQUARE\n";
+	cout << "Width: " << f.GetSquareWidth() << endl;
+	cout << "Perimeter: " << f.GetSquarePerimeter() << endl;
+	cout << "Area: " << f.GetSquareArea() << endl;
+
+	cout << "\nCIRCLE\n";
+	cout << "Radius: " << f.GetCircleRadius() << endl;
+	cout << "Perimeter: " << f.GetCirclePerimetr() << endl;
+	cout << "Area: " << f.GetCircleArea() << endl;
+
+}
