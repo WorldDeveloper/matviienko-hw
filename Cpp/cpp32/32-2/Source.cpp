5ec873ba59bea62791ac9e0cab19796b898dc58d
matviienkos@gmail.com <matviienkos@gmail.com@7c4d67bf-a6d6-bd9e-cbfa-6ceb9c08b31b>
2014-09-19 19:07:56 +0000

    cpp32-2 was added

---
+++ Cpp/cpp32/32-2/Source.cpp
@@ -0,0 +1,52 @@
+﻿/*
+Создать базовый класс список. Реализовать на базе списка стек и очередь с виртуальными функциями вставки и вытаскивания.
+*/
+
+#include <iostream>
+#include "List.h"
+#include "Queue.h"
+#include "Stack.h"
+using namespace std;
+
+void main()
+{
+	List<int> list;
+	Queue<int> queue;
+	Stack<int> stack;
+
+	List<int>* pList = &list;
+	List<int>* pQueue = &queue;
+	List<int>* pStack = &stack;
+	
+	for (int i = 0; i < 5; ++i)
+	{
+		list.Push(i);
+		queue.Push(i);
+		stack.Push(i);
+	}
+
+	cout << "List \tQueue \tStack\n";
+	for(int i = 0; i < 5; ++i)
+	{
+		cout << list.Pop() << "\t";
+		cout << queue.Pop() << "\t";
+		cout << stack.Pop() << "\n";
+	}
+
+	cout << "\n*******Pointers**********\n\n";
+	for (int i = 0; i < 5; ++i)
+	{
+		pList->Push(i);
+		pQueue->Push(i);
+		pStack->Push(i);
+	}
+
+	cout << "List \tQueue \tStack\n";
+	for (int i = 0; i < 5; ++i)
+	{
+		cout << pList->Pop() << "\t";
+		cout << pQueue->Pop() << "\t";
+		cout << pStack->Pop() << "\n";
+	}
+	cout << endl;
+}
