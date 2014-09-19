35be1f6293d6d4499e6fa9ef579e7b0fcb7561a2
matviienkos@gmail.com <matviienkos@gmail.com@7c4d67bf-a6d6-bd9e-cbfa-6ceb9c08b31b>
2014-09-19 15:17:42 +0000

    cpp27-3 was modified

--- trunk/Cpp/cpp27/27-3/Array.h
+++ trunk/Cpp/cpp27/27-3/Array.h
@@ -1,343 +1,252 @@
 ﻿#pragma once
-
-template <class T>
-struct Node
-{
-	T value;
-	Node* prev;
-	Node* next;
-	Node() : value(0), prev(nullptr), next(nullptr){}
-	Node(const T& data) : value(data), prev(nullptr), next(nullptr){}
-};
+#include <memory>
+using namespace std;
 
 template <class T>
 class Array
 {
-	Node<T>* mpHead;
-	Node<T>* mpTail;
-	int mCount;
-	int mCurIndex;
+	T* mArray;
+	int mUpperBound;
+	int mSize;
 	int mGrow;
 
-	T& operator[](const int index) const;
-	Node<T>* Item(const int index) const;
-	void PushBack(const T& value);
-	void RemoveLast();
+	void CreateArray(const int size);
+	void ReSize(const int size, const int grow=0);
 public:
-	Array() : mpHead(nullptr), mpTail(nullptr), mCount(0), mCurIndex(0), mGrow(0){}
+	Array(const int size=1, const int grow=1) 
+		: mSize(size), mUpperBound(-1), mGrow(grow)
+	{
+		if (size < 1 || grow < 1)
+		{
+			cout << "\nError: incorrect parametrs!";
+			exit(1);
+		}
+		CreateArray(size);
+	}
 	~Array();
-
-	Array(const Array& source);
-	Array& operator=(const Array& source);
-
-	int GetSize() const { return mCount; }
+	int GetSize() const { return mSize; }
 	void SetSize(const int size, const int grow = 1);
-	int GetUpperBound() const { return mCurIndex - 1; }
-	bool IsEmpty() const { return mCurIndex == 0; }
+	int GetUpperBound() const { return mUpperBound; }
+	bool IsEmpty() const { return mUpperBound == -1; }
 	void FreeExtra();
 	void RemoveAll();
-
-	T GetAt(const int index) const;
+	T & operator[](const int index);
+	T GetAt(int index);
 	void SetAt(const int index, const T& value);
-
-	void Add();
-	void Append(Array obj);
-	T* GetData() const;
-
-	bool InsertAt(const int index, const T& value);
-	bool RemoveAt(const int index);
+	void Add(const T& value);
+	void Append(const Array& appendedObj);
+	Array(const Array& source);
+	Array& operator=(const Array& source);
+	T* GetData() const { return mArray; }
+	void InsertAt(const int index, const T& value);
+	void RemoveAt(const int index);
 };
-
-template <class T>
-void Array<T>::SetSize(const int size, const int grow)
-{
-	mGrow = grow;
-
-	while (mCount < size)
-	{
-		PushBack(T());
-	}
-
-	while (mCount>size)
-	{
-		RemoveLast();
-	}
-}
-
-template <class T>
-void Array<T>::RemoveAll()
-{
-	while (mpTail)
-	{
-		Node<T>* tmp = mpTail;
-
-		if (mpTail->prev)
-		{
-			mpTail = mpTail->prev;
-		}
-		else
-		{
-			mpTail = mpHead = nullptr;
-		}
-		delete tmp;
-	}
-	mCount = 0;
-	mCurIndex = 0;
-}
 
 template <class T>
 Array<T>::~Array()
 {
-	while (mpTail)
-	{
-		RemoveLast();
-	}
+	delete[] mArray;
+	mArray = nullptr;
+	mSize = 0;
+	mUpperBound = -1;
+	mGrow = 1;
+}
 
-	mCount = 0;
-	mCurIndex = 0;
-	mGrow = 0;
+
+template<class T>
+void Array<T>::CreateArray(const int size)
+{
+	try{
+		mArray = new T[size];
+	}
+	catch (bad_alloc)
+	{
+		cout << "\nError: out of memory!\n\n";
+		exit(1);
+	}
 }
 
 template <class T>
-Array<T>::Array(const Array<T>& source)
+void Array<T>::ReSize(const int size, const int grow/*=0*/)
 {
-	mpHead = nullptr;
-	mpTail = nullptr;
-	mCount = 0;
-
-	Node<T>* tmp = source.mpHead;
-	for (int i = 0; i < source.mCount; ++i)
+	if (size < 0 || grow < 0)
 	{
-		PushBack(tmp->value);
-		tmp = tmp->next;
+		cout << "Error: incorrect parametrs!";
+		exit(1);
+	}
+	if (grow) mGrow = grow;
+
+	if ( mSize==size)	return;
+	else mSize = size;
+
+	if (mUpperBound + 1 > mSize)
+	{
+		mUpperBound = mSize - 1;
 	}
 
-	mCurIndex = source.mCurIndex;
-	mGrow = source.mGrow;
+	T* tmp = mArray;
+	CreateArray(mSize);
+	for (int i = 0; i <= mUpperBound; ++i)
+	{
+		mArray[i] = tmp[i];
+	}
+	delete[] tmp;
 }
 
 template <class T>
-Array<T>& Array<T>::operator=(const Array<T>& source)
+void Array<T>::SetSize(const int size, const int grow/*=1*/)
 {
-	if (this == &source) return *this; 
-	RemoveAll();
-
-	Node<T>* tmp = source.mpHead;
-	for (int i = 0; i < source.mCount; ++i)
+	if (size < 0 || grow < 0)
 	{
-		PushBack(tmp->value);
-		tmp = tmp->next;
+		cout << "Error: incorrect parametrs!";
+		exit(1);
 	}
-
-	mCurIndex = source.mCurIndex;
-	mGrow = source.mGrow;
-
-	return *this;
-}
-
-template <class T>
-void Array<T>::PushBack(const T& value)
-{
-	Node<T>* tmp = new Node<T>;
-	tmp->value = value;
-
-	if (!mpHead)
+	
+	if (mArray)
 	{
-		mpHead = tmp;
-		mpTail = tmp;
+		ReSize(size, grow);
 	}
 	else
 	{
-		tmp->prev = mpTail;
-		mpTail->next = tmp;
-		mpTail = tmp;
-	}
-	mCount++;
-}
-
-template <class T>
-void Array<T>::RemoveLast()
-{
-	if (mpTail)
-	{
-		Node<T>* tmp = mpTail;
-		mCount--;
-		if (mpTail->prev)
-		{
-			mpTail = mpTail->prev;
-			mpTail->next = nullptr;
-		}
-		else
-		{
-			mpTail = mpHead = nullptr;
-		}
-
-		if (mCurIndex > mCount) { mCurIndex = mCount; }
-
-		delete tmp;
+		mSize = size;
+		mUpperBound = -1;
+		mGrow = grow;
+		CreateArray(size);
 	}
 }
 
 template <class T>
 void Array<T>::FreeExtra()
 {
-	while (mCount > mCurIndex)
-	{
-		RemoveLast();
-	}
+	ReSize(mUpperBound + 1);
 }
 
 template <class T>
-Node<T>* Array<T>::Item(const int index) const
+void Array<T>::RemoveAll()
 {
-	if (index < 0 || index >= mCount) { exit(1); }
-
-	Node<T>* tmp = mpHead;
-	for (int i = 0; i < index; ++i)
-	{
-		tmp = tmp->next;
-	}
-
-	if (tmp) { return tmp; }
-	else { exit(1); }
+	this->~Array();
 }
 
 template <class T>
-T& Array<T>::operator[](const int index) const
+T& Array<T>::operator[](const int index)
 {
-	if (index < 0 || index >= mCount) { exit(1); }
-
-	Node<T>* tmp = mpHead;
-	for (int i = 0; i < index; ++i)
+	if (index >= 0 && index <= mUpperBound)
 	{
-		tmp = tmp->next;
-	}
-
-	if (tmp) { return tmp->value; }
-	else { exit(1); }
-}
-
-template <class T>
-T Array<T>::GetAt(const int index) const
-{
-	if (index < mCurIndex)
-	{
-		return this->operator[](index);  //return Item(index)->value;
+		return mArray[index];
 	}
 	else
 	{
+		cout << endl << "Error: out of range!" << endl;
 		exit(1);
 	}
 }
+
+template <class T>
+T Array<T>::GetAt(int index)
+{
+	return this->operator[](index);
+}
+
 
 template <class T>
 void Array<T>::SetAt(const int index, const T& value)
 {
-	if (index <= mCurIndex)
+	this->operator[](index) = value;
+}
+
+template <class T>
+void Array<T>::Add(const T& value)
+{
+	if (mUpperBound + 1 >= mSize)
 	{
-		this->operator[](index) = value;  // Item(index)->value = value;
-		if (index == mCurIndex && mCurIndex < mCount) { mCurIndex++; }
+		ReSize(mSize + mGrow);		
 	}
-	else
+	mUpperBound++;
+	mArray[mUpperBound] = value;
+}
+
+template<class T>
+void Array<T>::Append(const Array& appendedObj)
+{
+	if (appendedObj.IsEmpty()) return;
+
+	int newSize = mUpperBound + appendedObj.mUpperBound+2;
+	newSize += newSize%mGrow;
+	ReSize(newSize);
+
+	for (int i = 0; i <= appendedObj.GetUpperBound(); ++i)
 	{
+		Add(appendedObj.mArray[i]);
+	}
+}
+
+template <class T>
+Array<T>::Array(const Array<T>& source)
+{
+	mSize = source.mSize;
+	mUpperBound = source.mUpperBound;
+	mGrow = source.mGrow;
+	CreateArray(mSize);
+
+	for (int i = 0; i <= mUpperBound; ++i)
+	{
+		mArray[i] = source.mArray[i];
+	}
+}
+
+template <class T>
+Array<T>& Array<T>::operator=(const Array<T>& source)
+{
+	if (this == &source) return *this; 
+
+	if (mSize != source.mSize)
+	{
+		delete[] mArray;
+		CreateArray(source.mSize);
+	}
+
+	mSize = source.mSize;
+	mUpperBound = source.mUpperBound;
+	mGrow = source.mGrow;
+
+	for (int i = 0; i <= mUpperBound; ++i)
+	{
+		mArray[i] = source.mArray[i];
+	}
+
+	return *this;
+}
+
+template <class T>
+void Array<T>::InsertAt(const int index, const T& value)
+{
+	if (index<0 || index>mUpperBound)
+	{
+		cout << endl << "Error: out of range!" << endl;
 		exit(1);
 	}
+
+	if (mUpperBound + 1 == mSize) ReSize(mSize + mGrow);
+
+	for (int i = ++mUpperBound; i > index; --i)
+	{
+		mArray[i] = mArray[i - 1];
+	}
+	mArray[index] = value;
 }
 
 template <class T>
-void Array<T>::Add()
+void Array<T>::RemoveAt(const int index)
 {
-	do
+	if (index<0 || index>mUpperBound)
 	{
-		PushBack(T());
-	} while (mCount%mGrow);
-}
-
-template <class T>
-void Array<T>::Append(Array<T> obj)
-{
-	this->FreeExtra();
-	Node<T>* tmp = obj.mpHead;
-	while (tmp)
-	{
-		PushBack(tmp->value);
-		tmp = tmp->next;
+		cout << endl << "Error: out of range!" << endl;
+		exit(1);
 	}
 
-	mCurIndex += obj.mCurIndex;
-}
-
-template <class T>
-T* Array<T>::GetData() const
-{
-	T* pData = new T[mCurIndex];
-	for (int i = 0; i < mCurIndex; ++i)
+	for (int i = index; i <mUpperBound; ++i)
 	{
-		pData[i] = Item(i)->value;
+		mArray[i] = mArray[i + 1];
 	}
-
-	return pData;
-}
-
-template <class T>
-bool Array<T>::InsertAt(const int index, const T& value)
-{
-	if (index<0 || index>mCurIndex) { return false; }
-
-	Node<T>* insertedItem = new Node<T>;
-	insertedItem->value = value;
-	if (index <= mCount) { mCurIndex++; }
-
-	if (index == mCount)
-	{
-		PushBack(value);
-		return true;
-	}
-	else if (index == 0)
-	{
-		insertedItem->next = mpHead;
-		mpHead->prev = insertedItem;
-		mpHead = insertedItem;
-	}
-	else
-	{
-		Node<T>* oldItem = Item(index);
-		insertedItem->prev = oldItem->prev;
-		(oldItem->prev)->next = insertedItem;
-		insertedItem->next = oldItem;
-		oldItem->prev = insertedItem;
-
-		if (!insertedItem->next) { mpTail = insertedItem; }
-	}
-
-	mCount++;
-	return true;
-}
-
-template <class T>
-bool Array<T>::RemoveAt(const int index)
-{
-	if (index < 0 || index >= mCurIndex) { return false; }
-	mCurIndex--;
-	if (index == mCount - 1)
-	{
-		RemoveLast();
-		return true;
-	}
-	else if (index == 0)
-	{
-		Node<T>* tmp = mpHead;
-		mpHead = mpHead->next;
-		mpHead->prev = nullptr;
-		delete tmp;
-	}
-	else
-	{
-		Node<T>* oldNode = Item(index);
-		(oldNode->prev)->next = oldNode->next;
-		(oldNode->next)->prev = oldNode->prev;
-		delete oldNode;
-	}
-
-	mCount--;
+	mUpperBound--;
 }
