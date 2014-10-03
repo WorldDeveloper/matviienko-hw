bfb0c22705fc8947cffdbff7510cb8ec786acaf8
matviienkos@gmail.com <matviienkos@gmail.com@7c4d67bf-a6d6-bd9e-cbfa-6ceb9c08b31b>
2014-10-03 13:55:52 +0000

    cpp33-2 was added

---
+++ Cpp/cpp33/33-2/Source.cpp
@@ -0,0 +1,65 @@
+﻿/*
+Перепишите класс stack одного из предыдущих домашних заданий таким образом,
+чтобы генерировать исключения для такого количества условий, какое Вы
+считаете приемлемым. (Например, невозможность выделить нужное количество
+памяти, переполнение стека и т.д.)
+*/
+#include <iostream>
+#include "Stack.h"
+using namespace std;
+
+
+void Fill(Stack<int>& stack, const int size);
+void Show(Stack<int>& stack, const int size);
+
+void main()
+{
+	try
+	{
+		Stack<int> stack;
+
+		cout << "Stack is filling...\n";
+		Fill(stack, 3);
+
+		cout << "\nStack is clearing...\n";
+		stack.Clear();
+
+		cout << "\nStack is popping...\n";
+		if (!stack.Empty()) Show(stack, 10);
+		else cout << "\t stack is empty\n";
+
+		cout << "\nStack is filling...\n";
+		Fill(stack, 3);
+
+		cout << "\nStack is popping...\n";
+		Show(stack, 4);
+
+	}
+	catch (bad_alloc)
+	{
+		cout << "\nError: out of memory.\n";
+	}
+	catch (char* e)
+	{
+		cout << "\nError: " << e << ".\n";
+	}
+}
+
+void Fill(Stack<int>& stack, const int size)
+{
+	for (int i = 0; i < size; ++i)
+	{
+		stack.PushBack(i);
+		cout << "\tpush " << i << endl;
+	}
+	cout << endl;
+}
+
+void Show(Stack<int>& stack, const int size)
+{
+	for (int i = 0; i < size; ++i)
+	{
+		cout << "\tpop " << stack.PopBack() << endl;
+	}
+	cout << endl;
+}
