844f9cea3d6a97b5774d0a9cf9524a43284691ab
matviienkos@gmail.com <matviienkos@gmail.com@7c4d67bf-a6d6-bd9e-cbfa-6ceb9c08b31b>
2014-10-04 18:50:43 +0000

    cpp35-2 was added

---
+++ trunk/Cpp/cpp35/35-2/Source.cpp
@@ -0,0 +1,57 @@
+﻿/*
+Заполнить двухмерный вектор таблицей умножения и выввести его в выходной
+поток.
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+template <class T>
+ostream& operator<<(ostream& stream, vector<T>& output);
+
+template <class T>
+ostream& operator<<(ostream& stream, vector<vector<T>>& output);
+
+void main()
+{
+	vector<vector<int>> multiplicationTable;
+	const int m = 9;
+	const int n = 9;
+	multiplicationTable.resize(m);
+
+	for (int i = 0; i <m; ++i)
+	{
+		multiplicationTable[i].resize(n);
+		for (int j = 0; j <n; ++j)
+		{
+			multiplicationTable[i][j] = (i+1)*(j+1);
+		}
+	}
+
+	cout << multiplicationTable << endl;
+}
+
+template <class T>
+ostream& operator<<(ostream& stream, vector<T>& output)
+{
+
+	for (auto& i : output)
+	{
+		stream << i << "\t";
+	}
+
+	return stream;
+}
+
+template <class T>
+ostream& operator<<(ostream& stream, vector<vector<T>>& output)
+{
+	
+	for (int i = 0; i < output.size();++i)
+	{
+		stream << output[i] << endl;
+	}
+
+	return stream;
+}
