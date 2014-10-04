9d902959a0c4c07323e50a2a0280d809cc2e2e5c
matviienkos@gmail.com <matviienkos@gmail.com@7c4d67bf-a6d6-bd9e-cbfa-6ceb9c08b31b>
2014-10-04 18:09:57 +0000

    cpp35-1 was added

---
+++ Cpp/cpp35/35-1/Source.cpp
@@ -0,0 +1,34 @@
+﻿/*
+Заполнить вектор длинной 10 квадратами целых чисел и вывести его в выходной
+поток.
+*/
+#include <iostream>
+#include <vector>
+using namespace std;
+
+template <class T>
+ostream& operator<<(ostream& stream, vector<T>& output);
+
+void main()
+{
+	vector<int> v;
+	for (int i = 1; i < 11; ++i)
+	{
+		v.push_back(i*i);
+	}
+
+	cout << v << endl;
+}
+
+template <class T>
+ostream& operator<<(ostream& stream, vector<T>& output)
+{
+	stream << "[";
+	for (auto& i : output)
+	{
+		stream << i << " ";
+	}
+	stream << "\b]";
+
+	return stream;
+}
