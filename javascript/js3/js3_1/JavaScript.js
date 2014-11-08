c7a11bad4400d74b39c1a776804dad664e7c0aca
matviienkos@gmail.com <matviienkos@gmail.com@7c4d67bf-a6d6-bd9e-cbfa-6ceb9c08b31b>
2014-11-08 17:14:43 +0000

    js3_1 was added

---
+++ javascript/js3/js3_1/JavaScript.js
@@ -0,0 +1,26 @@
+﻿
+function Rollover(paths) {
+    this.mPaths = paths;
+    this.mLoadedImgCount = 0;
+
+    this.OnMouseOver = onMouseOver;
+    this.OnMouseOut = onMouseOut;
+    this.CountImages = function () { this.mLoadedImgCount++; }
+
+    this.mImages = new Array();
+    for (var i = 0; i < this.mPaths.length; ++i) {
+        this.mImages[i] = new Image(160, 110);
+        this.mImages[i].onload = this.CountImages();
+        this.mImages[i].src = "rollovers/" + this.mPaths[i];
+    }
+}
+
+function onMouseOver(elem, index) {
+    if (this.mLoadedImgCount == this.mPaths.length)
+        elem.src = this.mImages[index].src;
+}
+
+function onMouseOut(elem, index) {
+    if (this.mLoadedImgCount == this.mPaths.length)
+        elem.src = "images/" + this.mPaths[index];
+}
