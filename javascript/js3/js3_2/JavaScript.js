68cfa0b73ddde1b02a7e1d3f5191ee61bdb2f8fd
matviienkos@gmail.com <matviienkos@gmail.com@7c4d67bf-a6d6-bd9e-cbfa-6ceb9c08b31b>
2014-11-11 20:46:56 +0000

    js3_2 was added

---
+++ javascript/js3/js3_2/JavaScript.js
@@ -0,0 +1,19 @@
+﻿    
+function format (n){
+    return n<10 ? "0"+n : n;
+}
+        
+function updateClock() {
+    var weekday = [];
+    weekday[0] = "Sun";
+    weekday[1] = "Mon";
+    weekday[2] = "Tue";
+    weekday[3] = "Wed";
+    weekday[4] = "Thu";
+    weekday[5] = "Fri";
+    weekday[6] = "Sat";
+
+    var d = new Date ();
+    clock.innerHTML = "<span class='clocksTime'>" + format(d.getHours()) + ":" + format(d.getMinutes()) + ":" + format(d.getSeconds()) +"</span>"+
+        "<br /><span class='clocksDate'>" + weekday[d.getDay()] + " " + format(d.getDate()) + "." + format(d.getMonth() + 1) + "." + d.getFullYear() + "</span>";
+}
