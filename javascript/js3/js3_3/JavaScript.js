dd47765de6da30776c31e616e8f80c17b9bb2022
matviienkos@gmail.com <matviienkos@gmail.com@7c4d67bf-a6d6-bd9e-cbfa-6ceb9c08b31b>
2014-11-14 15:57:22 +0000

    js3_3 was added

---
+++ javascript/js3/js3_3/JavaScript.js
@@ -0,0 +1,25 @@
+﻿
+function Animation(elem) {
+    this.obj = elem;
+    this.pointerX = 0;
+    this.pointerY = 0;
+    this.R = 30;
+    this.a = 0;
+    this.da = 3 * Math.PI / 180;
+    this.delay = 30;
+    var self = this;
+
+    this.setCursorXY = function (e) {
+        self.pointerX = (window.Event) ? e.pageX : event.clientX + (document.documentElement.scrollLeft ? document.documentElement.scrollLeft : document.body.scrollLeft);
+        self.pointerY = (window.Event) ? e.pageY : event.clientY + (document.documentElement.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop);
+       
+    }
+    document.onmousemove = this.setCursorXY;
+   
+    this.moveElement = function () {
+        this.obj.style.left = (this.pointerX + this.R * Math.cos(this.a)) + "px";
+        this.obj.style.top = (this.pointerY + this.R * Math.sin(this.a)) + "px";
+        this.a += this.da;
+    }   
+    setInterval(function () { return self.moveElement(); }, this.delay);
+}
