2634257723eed4887bdd5106d91b040c29ba4bbb
matviienkos@gmail.com <matviienkos@gmail.com@7c4d67bf-a6d6-bd9e-cbfa-6ceb9c08b31b>
2014-11-14 20:25:57 +0000

    js5 was added

---
+++ javascript/js5/JavaScript.js
@@ -0,0 +1,13 @@
+﻿
+function CreateWnd(path) {
+    var wndWidth=640;
+    var wndHeight=480;
+    var wndLeft=(screen.availWidth-wndWidth)/2;
+    var wndTop = (screen.availHeight - wndHeight) / 2;
+
+    var wnd = window.open("", "", "width=" + wndWidth + ", height=" + wndHeight + ", left=" + wndLeft + ", top=" + wndTop + ", resizable=0");
+
+    wnd.document.open();
+    wnd.document.write("<html><body style='margin:0;'><img src='" + path + "' /></body></html>");
+    wnd.document.close();
+}
