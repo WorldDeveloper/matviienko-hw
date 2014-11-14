d8c4360c0427c0f334e90db14e48428d93439fe4
matviienkos@gmail.com <matviienkos@gmail.com@7c4d67bf-a6d6-bd9e-cbfa-6ceb9c08b31b>
2014-11-14 20:22:04 +0000

    js4 was added

---
+++ trunk/javascript/js4/JavaScript.js
@@ -0,0 +1,7 @@
+﻿var redirPages = ["bolt.html", "fj_09.html", "fz6r.html", "stryker.html", "super_tenere.html", "vmax.html", "yzf_r1.html", "yzf_r1m.html", "yzf_r3.html", "yzf_r6.html"];
+
+var randomIndex = Math.floor(Math.random() * 10);
+location.href = redirPages[randomIndex];
+
+
+
