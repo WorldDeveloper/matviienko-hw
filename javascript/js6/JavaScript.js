15c5d270ea9c958063b23716061d61e0c0e87456
matviienkos@gmail.com <matviienkos@gmail.com@7c4d67bf-a6d6-bd9e-cbfa-6ceb9c08b31b>
2014-11-15 00:08:30 +0000

    js6 was added

---
+++ javascript/js6/JavaScript.js
@@ -0,0 +1,6 @@
+﻿
+function OpenUrl(){
+    if (navigationBar.newWindow.checked) window.open(navigationBar.destUrl.value);
+    else document.getElementById("mainFrame").src = navigationBar.destUrl.value;
+}
+
