75b961e33b6853a1477f0d36c19b9cebed05784d
matviienkos@gmail.com <matviienkos@gmail.com@7c4d67bf-a6d6-bd9e-cbfa-6ceb9c08b31b>
2014-11-15 22:03:49 +0000

    js7_2 was added

---
+++ trunk/javascript/js7/js7_2/JavaScript.js
@@ -0,0 +1,7 @@
+﻿function checkEmail(text) {
+    var rExp = /^[a-z][a-z0-9\-]{2,9}@(([a-z0-9]+)|([a-z0-9]+\.[a-z0-9]+))\.[a-z]{2,3}$/;
+    if (rExp.test(text))
+        alert("Thanks!");
+    else
+        alert("Incorrect input. Please try again.");
+}
