124eb05379c1b9f55265eac17706602a4bbae5f9
matviienkos@gmail.com <matviienkos@gmail.com@7c4d67bf-a6d6-bd9e-cbfa-6ceb9c08b31b>
2014-11-15 21:31:06 +0000

    js7_1 was added

---
+++ javascript/js7/js7_1/JavaScript.js
@@ -0,0 +1,12 @@
+﻿function checkSurname(text) {
+
+    var capLetter = "[A-ZА-ЯЁІЇЄ']";
+    var smallLetter = "[a-zа-яёіїє']";
+    var re = "^" + capLetter + smallLetter + "+ ((" + capLetter + "\.)|(" + capLetter + ")){2}$"
+
+    var rExp = new RegExp(re, "");
+    if (rExp.test(text))
+        alert("Thanks, "+text+"!");
+    else
+        alert("Incorrect input. Please try again.");
+}
