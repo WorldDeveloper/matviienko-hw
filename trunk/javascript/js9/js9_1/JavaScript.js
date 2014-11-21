2ec1330fcd1b5d53f47faf032711e66dca8a79a7
matviienkos@gmail.com <matviienkos@gmail.com@7c4d67bf-a6d6-bd9e-cbfa-6ceb9c08b31b>
2014-11-21 20:22:18 +0000

    js9_1 was added

---
+++ trunk/javascript/js9/js9_1/JavaScript.js
@@ -0,0 +1,31 @@
+﻿function DropdownList(elem) {
+
+    var child = elem.firstElementChild;
+    style = window.getComputedStyle(child);
+    var displayStyle = style.getPropertyValue('display');
+
+    if (displayStyle == "none") {
+        elem.style.listStyleImage = 'url("img/minus.png")';
+        child.style.display = "block";       
+    }
+    else{
+        elem.style.listStyleImage = 'url("img/plus.png")';
+        child.style.display = "none";
+    }
+}
+
+function HideExpandText(elem) {
+
+    var child = elem.previousElementSibling;
+    style = window.getComputedStyle(child);
+    var displayStyle = style.getPropertyValue('display');
+
+    if (displayStyle == "none") {
+        elem.textContent='Hide text';
+        child.style.display = "block";
+    }
+    else {
+        elem.textContent = 'Expand text';
+        child.style.display = "none";
+    }
+}
