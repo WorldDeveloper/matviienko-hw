98bf256e093ba699502467ac910dbef1a3d495fc
matviienkos@gmail.com <matviienkos@gmail.com@7c4d67bf-a6d6-bd9e-cbfa-6ceb9c08b31b>
2014-11-22 10:29:41 +0000

    js9_2_1 was added

---
+++ trunk/javascript/js9/js9_2_1/JavaScript.js
@@ -0,0 +1,29 @@
+﻿function CreateList() {
+    document.open();
+    document.write("<ul id='toSort'>");
+    for (var i = 0; i < 10; ++i) {
+        var randomNumber = Math.floor(Math.random() * 10);
+        document.write("<li>item " + randomNumber + "</li>");
+    }
+    document.write("</ul>");
+    document.close();
+}
+
+function SortElements() {
+    var list = document.getElementById('toSort');
+    var childCount = list.childElementCount;
+
+    while (childCount > 1)
+    {
+        childCount--;
+        for (var item = 0; item < childCount; ++item)
+        {
+            var current = list.childNodes[item];
+            var next = list.childNodes[item + 1];
+
+            if (current.textContent > next.textContent) {
+                list.insertBefore(next, current);
+            }
+        }
+    }
+}
