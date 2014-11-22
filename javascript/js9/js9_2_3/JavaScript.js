1693b01598f24b8e63701bb8e7b729eb9c98348d
matviienkos@gmail.com <matviienkos@gmail.com@7c4d67bf-a6d6-bd9e-cbfa-6ceb9c08b31b>
2014-11-22 16:05:48 +0000

    js9_2_3 was added

---
+++ javascript/js9/js9_2_3/JavaScript.js
@@ -0,0 +1,25 @@
+﻿function DropdownList(elem) {
+
+    var child = elem.firstElementChild;
+    var parent = elem.parentElement;
+
+    for (var i = 0; i < elem.parentElement.childElementCount; ++i) {      
+
+        if (elem.parentElement.children[i].childElementCount!=0 && elem.parentElement.children[i] != elem ) {            
+            elem.parentElement.children[i].style.listStyleImage = 'url("img/plus.png")';
+            elem.parentElement.children[i].firstElementChild.style.display = "none";
+        }
+    }
+
+    style = window.getComputedStyle(child);
+    var displayStyle = style.getPropertyValue('display');
+
+    if (displayStyle == "none") {
+        elem.style.listStyleImage = 'url("img/minus.png")';
+        child.style.display = "block";       
+    }
+    else{
+        elem.style.listStyleImage = 'url("img/plus.png")';
+        child.style.display = "none";
+    }
+}
