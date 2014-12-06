05060800b5b64e0f498527876e8ae7e9a4df8a70
matviienkos@gmail.com <matviienkos@gmail.com@7c4d67bf-a6d6-bd9e-cbfa-6ceb9c08b31b>
2014-12-06 14:48:45 +0000

    wapi3_2 was modified

--- trunk/Cpp/wapi3/wapi3_2/TicTacToeGame.h
+++ trunk/Cpp/wapi3/wapi3_2/TicTacToeGame.h
@@ -7,13 +7,20 @@ int x2;
 int y2;
 Line():x1(0), y1(0), x2(0), y2(0){}
 void Clear()
-{
-	x1=0;
-	y1=0;
-	x2=0;
-	y2=0;
-}
+	{
+		x1=0;
+		y1=0;
+		x2=0;
+		y2=0;
+	}
 };
+
+	struct Move{
+		int value;
+		int row;
+		int col;
+		Move():value(0), row(0), col(0){}
+	};
 
 
 
@@ -33,7 +40,7 @@ public:
 	TicTacToeGame();
 	bool HumanMove(const int row, const int col);
 	bool ComputerMove(int& row, int& col);
-	void RandomMove(int& row, int& col);
+	void RandomMove(Move& ramdnomMove);
 	bool CanWin(const int mark);
 	bool SquareIsEmpty(const int row, const int col) const {return !mBoard[row][col];}
 	bool BoardIsFull() const {return !mEmptySquaresNumber;}
@@ -41,7 +48,8 @@ public:
 	Line GetResult() const {return mWinLine;}
 	void Reset();
 	bool CheckVictory(const int row, const int col, const int criteria);
-	bool BestMove(const int mark);
+	bool BestMove(Move& bestMove, const int playersMark);
+	void FirstMove(Move& firstMove);
 
 	~TicTacToeGame();
 };
