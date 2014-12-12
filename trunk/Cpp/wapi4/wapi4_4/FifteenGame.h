a3813b54361cf8e5bd262bbd853fc89f17b9d08c
matviienkos@gmail.com <matviienkos@gmail.com@7c4d67bf-a6d6-bd9e-cbfa-6ceb9c08b31b>
2014-12-12 01:16:21 +0000

    wapi4_4 was added

---
+++ trunk/Cpp/wapi4/wapi4_4/FifteenGame.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <vector>
+#include <string>
+
+
+class FifteenGame
+{
+	std::vector<int> mTile;
+	int mMissingTileIndex;
+	const int mTilesCount;
+	void Shuffle();
+public:
+	FifteenGame();
+	std::wstring GetTile(const int index) const;
+	int GetMissingTile() const { 		return mMissingTileIndex; 	}
+	int GetTilesCount() const { return mTilesCount; }
+	bool HasSolution() const;
+	bool IsFinished() const;
+	bool SwapTile(const int index);
+	void Randomize();
+};
+
---
+++ trunk/Cpp/wapi4/wapi4_4/Source.cpp
@@ -0,0 +1,16 @@
+﻿/*
+Написать игру «Пятнашки», учитывая следующие требования:
+- предусмотреть автоматическую перестановку «пятнашек» в начале новой игры;
+- выводить время, за которое пользователь окончил игру;
+- предусмотреть возможность начать новую игру.
+
+*/
+
+#include "FifteenDlg.h"
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpszCmdLine, int CmdShow)
+{
+	//try****	
+	FifteenDlg dlg;
+		return DialogBox(hInstance, MAKEINTRESOURCE(IDD_FIFTEEN), NULL, FifteenDlg::DlgProc);
+}
