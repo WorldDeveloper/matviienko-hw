fc002c9e13a183b89b39671a159327b9163350a9
matviienkos@gmail.com <matviienkos@gmail.com@7c4d67bf-a6d6-bd9e-cbfa-6ceb9c08b31b>
2014-12-13 18:37:59 +0000

    wapi5_3 was added

---
+++ trunk/Cpp/wapi5/wapi5_3/Source.cpp
@@ -0,0 +1,11 @@
+﻿/*
+Написать приложение, формирующее список логических дисков, которые доступны в данный момент
+*/
+
+#include "LogicalDrivesDlg.h"
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpszCmdLine, int CmdShow)
+{
+	LogicalDrivesDlg dlg;
+	return DialogBox(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, LogicalDrivesDlg::DlgProc);
+}
