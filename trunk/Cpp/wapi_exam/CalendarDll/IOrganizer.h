650a53f81e33f988bf2f6dd721c597d8f39d68bb
matviienkos@gmail.com <matviienkos@gmail.com@7c4d67bf-a6d6-bd9e-cbfa-6ceb9c08b31b>
2014-12-26 16:09:14 +0000

    wapi_examinationWork was modified: CalendarDll was added

---
+++ trunk/Cpp/wapi_exam/CalendarDll/IOrganizer.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <string>
+
+class IOrganizer
+{
+public:
+	virtual std::wstring GetPluginName() const = 0; 
+	virtual void SetWindow(HWND destWindow) = 0;
+	virtual bool AddItem() = 0;
+	virtual bool EditItem(const int id) = 0;
+	virtual bool DeleteItem(const int id) = 0;
+
+	virtual void ShowSingleItem(const int id) const = 0;
+	virtual void ShowAllItems() const = 0;
+};
