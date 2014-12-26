1b51f760f85944895b3c83b61dd8a7835259f353
matviienkos@gmail.com <matviienkos@gmail.com@7c4d67bf-a6d6-bd9e-cbfa-6ceb9c08b31b>
2014-12-26 16:06:46 +0000

    wapi_examinationWork was modified

--- Cpp/wapi_exam/visual_organizer/Header.h
+++ Cpp/wapi_exam/visual_organizer/Header.h
@@ -3,7 +3,15 @@
 #include <windowsx.h>
 #include <Windows.h>
 #include "resource.h"
+#include <vector>
+#include <string>
+
+
 
 #include <CommCtrl.h>
 #pragma comment(lib,"comctl32.lib")
 #pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
+
+
+#include <io.h>
