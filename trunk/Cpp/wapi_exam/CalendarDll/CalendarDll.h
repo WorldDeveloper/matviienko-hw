e5c30a6b4bb7cfe0615018582a99b21046d574ce
matviienkos@gmail.com <matviienkos@gmail.com@7c4d67bf-a6d6-bd9e-cbfa-6ceb9c08b31b>
2014-12-28 10:54:19 +0000

    wapi_examinationWork was modified

--- trunk/Cpp/wapi_exam/CalendarDll/CalendarDll.h
+++ trunk/Cpp/wapi_exam/CalendarDll/CalendarDll.h
@@ -2,12 +2,46 @@
 #include <Windows.h>
 #include "IOrganizer.h"
 #include <string>
+#include <vector>
+#include <ctime>
+
+
+struct Event
+{
+	std::wstring mName;
+	std::wstring mDescription;
+	std::wstring mWhere;
+	tm mFromDate;
+	tm mToDate;
+
+	Event(std::wstring name, std::wstring description, std::wstring where, tm fromDate, tm toDate)
+	{
+		mFromDate = { 0 };
+		mToDate = { 0 };
+
+		if (name.empty()) throw "empty event name";
+
+		time_t difference = mktime(&toDate) - mktime(&fromDate);
+		if (!fromDate.tm_year || !toDate.tm_year || difference<0) throw "incorrect date";
+
+		mName = name;
+		mDescription = description;
+		mWhere = where;
+		mFromDate = fromDate;
+		mToDate = toDate;
+	}
+};
+
+
 
 
 class Calendar : public IOrganizer
 {
 	HWND mPluginWindow;
 	HWND mhList;
+
+	std::vector<Event> mEvents;
+	char* mDBname = "events";
 
 	std::wstring GetPluginName() const { return L"Calendar"; }
 	HWND GetPluginWindow() const { return mPluginWindow; }
