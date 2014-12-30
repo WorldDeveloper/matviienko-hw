7d1f97563ac411d8fb407a890f45e354f84a8f96
matviienkos@gmail.com <matviienkos@gmail.com@7c4d67bf-a6d6-bd9e-cbfa-6ceb9c08b31b>
2014-12-30 10:38:53 +0000

    wapi_examinationWork was modified

--- trunk/Cpp/wapi_exam/AlarmDll/AlarmDlg.cpp
+++ trunk/Cpp/wapi_exam/AlarmDll/AlarmDlg.cpp
@@ -1,15 +1,15 @@
 #include "AlarmDlg.h"
 
-DlgCracker* DlgCracker::ptr = nullptr;
+AlarmDlg* AlarmDlg::ptr = nullptr;
 
-DlgCracker::DlgCracker(std::wstring action)
+AlarmDlg::AlarmDlg(std::wstring action)
 {
 	ptr = this;
 	mAction = action;
 	mAlarm = 0;
 }
 
-BOOL CALLBACK DlgCracker::DlgProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+BOOL CALLBACK AlarmDlg::DlgProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
 	switch (message)
 	{
@@ -22,7 +22,7 @@ BOOL CALLBACK DlgCracker::DlgProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM
 }
 
 
-BOOL DlgCracker::Cls_OnInitDialog(HWND hWnd, HWND hwndFocus, LPARAM lParam)
+BOOL AlarmDlg::Cls_OnInitDialog(HWND hWnd, HWND hwndFocus, LPARAM lParam)
 {
 	mhDlg = hWnd;
 	mhYear = GetDlgItem(hWnd, IDC_YEAR);
@@ -31,19 +31,102 @@ BOOL DlgCracker::Cls_OnInitDialog(HWND hWnd, HWND hwndFocus, LPARAM lParam)
 	mhHour = GetDlgItem(hWnd, IDC_HOUR);
 	mhMin = GetDlgItem(hWnd, IDC_MIN);
 
+	time_t rawtime;
+	time(&rawtime);
+	
+	SetDateTime();
+
 	//if (mNote.size()) SetWindowText(mhEdit, mNote.c_str());
 	//if (mAction == L"View") EnableWindow(mhEdit, FALSE);
 
 	return TRUE;
 }
 
-void DlgCracker::Cls_OnCommand(HWND hWnd, int id, HWND hwndCtl, UINT codeNotify)
+void AlarmDlg::Cls_OnCommand(HWND hWnd, int id, HWND hwndCtl, UINT codeNotify)
 {
-
+	if (codeNotify == CBN_SELCHANGE)
+	{
+		//SetDay(2014, 12, 1);
+	}
 }
 
-void DlgCracker::Cls_OnClose(HWND hWnd)
+void AlarmDlg::Cls_OnClose(HWND hWnd)
 {
 
 	EndDialog(mhDlg, 0);
+}
+
+int AlarmDlg::LastDayInMonth(const int year, const int month) const
+{
+	if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
+		return 31;
+	else if (month == 4 || month == 6 || month == 9 || month == 11)
+		return 30;
+	else {
+		if (year % 4 == 0) {
+			if (year % 100 == 0) {
+				if (year % 400 == 0)
+					return 29;
+				return 28;
+			}
+			return 29;
+		}
+		return 28;
+	}
+}
+
+bool  AlarmDlg::SetMonth(const int month)
+{
+	if (month<1 || month>12) return false;
+
+	wchar_t* months[12] = { L"January", L"February", L"March", L"April", L"May", L"June", L"July", L"August", L"September", L"October", L"November", L"December" };
+	for (int i = 0; i < 12; ++i)
+	{
+		int ind = SendMessage(mhMonth, CB_ADDSTRING, 0, (LPARAM)months[i]);
+		SendMessage(mhMonth, CB_SETITEMDATA, ind, i);
+	}
+	SendMessage(mhMonth, CB_SETCURSEL, month-1, 0);
+
+	return true;
+}
+
+bool  AlarmDlg::SetDay(const int year, const int month, const int curDay)
+{
+	if (year <= 0 || month<1 || month>12 || curDay<1) return false;
+
+	const int daysInMonth = LastDayInMonth(year, month);
+	if (curDay>daysInMonth) return false;
+
+	for (int i = 1; i < daysInMonth + 1; ++i)
+	{
+		const int ind = SendMessage(mhDay, CB_ADDSTRING, 0, (LPARAM)(std::to_wstring(i).c_str()));
+		SendMessage(mhDay, CB_SETITEMDATA, ind, i);
+	}
+	SendMessage(mhDay, CB_SETCURSEL, curDay-1, 0);
+
+	return true;
+}
+
+void AlarmDlg::SetDateTime(const time_t dateTime/*=0*/)
+{
+	time_t rawtime;
+	struct tm * moment;
+	
+	if (dateTime) rawtime = dateTime;
+	time(&rawtime);	
+	moment = localtime(&rawtime);
+
+	SetWindowText(mhYear, std::to_wstring(moment->tm_year + 1900).c_str());
+	SetMonth(moment->tm_mon + 1);
+	SetDay(moment->tm_year + 1900, moment->tm_mon + 1, moment->tm_mday);
+
+	std::wstring hour;
+	if (moment->tm_hour < 10) hour = L"0";
+	hour+=std::to_wstring(moment->tm_hour);
+	SetWindowText(mhHour, hour.c_str());
+
+	std::wstring min;
+	if (moment->tm_min < 10) min = L"0";
+	min += std::to_wstring(moment->tm_min);
+	SetWindowText(mhMin, min.c_str());
 }
--- trunk/Cpp/wapi_exam/AlarmDll/AlarmDlg.h
+++ trunk/Cpp/wapi_exam/AlarmDll/AlarmDlg.h
@@ -7,7 +7,7 @@
 
 #include "resource.h"
 
-class DlgCracker
+class AlarmDlg
 {
 	HWND mhDlg;
 	HWND mhYear;
@@ -18,13 +18,20 @@ class DlgCracker
 
 	time_t mAlarm;
 	std::wstring mAction;
-public:
-	DlgCracker(std::wstring action);
 
-	static DlgCracker* ptr;
+	int LastDayInMonth(const int year, const int month) const;
+	bool  SetMonth(const int month);
+	bool  SetDay(const int year,  const int month, const int curDay);
+	void SetDateTime(const time_t dateTime=0);
+
+public:
+	AlarmDlg(std::wstring action);
+
+	static AlarmDlg* ptr;
 	static BOOL CALLBACK DlgProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
 
 	void Cls_OnClose(HWND hWnd);
 	BOOL Cls_OnInitDialog(HWND hWnd, HWND hwndFocus, LPARAM lParam);
 	void Cls_OnCommand(HWND hWnd, int id, HWND hwndCtl, UINT codeNotify);
+
 };
