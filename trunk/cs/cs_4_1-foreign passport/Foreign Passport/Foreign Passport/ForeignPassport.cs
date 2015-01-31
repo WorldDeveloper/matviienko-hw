cc22f596b2dd31d356e83edc25f0e87566913fc7
matviienkos@gmail.com <matviienkos@gmail.com@7c4d67bf-a6d6-bd9e-cbfa-6ceb9c08b31b>
2015-01-31 15:27:17 +0000

    cs4_1 was added (foreign passport)

---
+++ trunk/cs/cs_4_1-foreign passport/Foreign Passport/Foreign Passport/ForeignPassport.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PassportNS;
+
+namespace Foreign_PassportNS
+{
+    class ForeignPassport:Passport
+    {
+        public string Type { get; private set; }
+        public string CountryCode { get; private set; }
+        public string Nationality { get; private set; }
+        public string PersonalNo { get; private set; }
+        public DateTime ExpiryDate { get; private set; }
+        public string Visas { get; private set; }
+
+        public ForeignPassport(
+            string type,
+            string countryCode,
+            string nationality,
+            string personalNo, 
+            DateTime expiryDate,
+            string visas,
+            string passportNo,
+            string surname,
+            string name,
+            string patronymicName,
+            DateTime birthDate,
+            string birthPlace,
+            string sex,
+            string authority,
+            DateTime issueDate,
+            string residencePlace,
+            string familyStatus="",
+            string specialNotes="")
+            :base( passportNo, surname, name, patronymicName,
+             birthDate, birthPlace, sex, authority, issueDate,
+            residencePlace,  familyStatus="",  specialNotes="")
+        {
+            Type = type;
+            CountryCode = countryCode;
+            Nationality = nationality;
+            PersonalNo = personalNo;
+            ExpiryDate = expiryDate;
+            Visas = visas;
+        }
+
+
+        public void AddVisa(string visa) { Visas += "\n\t" + visa; }
+
+        public override  void Print()
+        {
+            Console.WriteLine("Type: {0}", Type);
+            Console.WriteLine("Country code: {0}", CountryCode);
+            Console.WriteLine("Passport No: {0}", PassportNo);
+            Console.WriteLine("Surname: {0}", Surname);
+            Console.WriteLine("Name: {0}", Name);
+            Console.WriteLine("Nationality: {0}", Nationality);
+            Console.WriteLine("Date of birth: {0}", BirthDate.ToString("dd.MM.yyyy"));
+            Console.WriteLine("Nationality: {0}", Nationality);
+            Console.WriteLine("Personal No: {0}", PersonalNo);
+            Console.WriteLine("Expiry date: {0}", ExpiryDate);
+            Console.WriteLine("Visas: {0}", Visas);
+            Console.WriteLine("Personal No:{0}", PersonalNo);
+            Console.WriteLine("Sex: {0}", Sex); 
+            Console.WriteLine("Place of birth: {0}", BirthPlace);
+            Console.WriteLine("Authority: {0}", Authority);
+            Console.WriteLine("Date of issue: {0}", IssueDate.ToString("dd.MM.yyyy"));
+            Console.WriteLine("Date of expiry: {0}", ExpiryDate.ToString("dd.MM.yyyy"));
+            Console.WriteLine("Special notes: {0}", SpecialNotes);
+            Console.WriteLine("Visas: {0}", Visas);       
+        }
+    }
+}
---
+++ trunk/cs/cs_4_1-foreign passport/Foreign Passport/Foreign Passport/Passport.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassportNS
+{
+    class Passport
+    {
+        public string PassportNo { get; private set; }
+        public string Surname { get; private set; }
+        public string Name { get; private set; }
+        public string PatronymicName { get; private set; }
+        public DateTime BirthDate { get; private set; }
+        public  string BirthPlace { get; private set; }
+        public string Sex { get; private set; }
+        public string Authority { get; private set; }
+        public DateTime IssueDate { get; private set; }
+        public string SpecialNotes { get; private set; }
+        public string FamilyStatus { get; private set; }
+        public string ResidencePlace { get; private set; }
+       
+        public Passport(string passportNo, string surname, string name, string patronymicName,
+            DateTime birthDate, string birthPlace, string sex, string authority, DateTime issueDate,
+            string residencePlace, string familyStatus="", string specialNotes="")
+        {
+            PassportNo = passportNo;
+            Surname=surname;
+            Name = name;
+            PatronymicName = patronymicName;
+            BirthDate = birthDate;
+            BirthPlace = birthPlace;
+            Sex = sex;
+            Authority = authority;
+            IssueDate = issueDate;
+            SpecialNotes = specialNotes;
+            FamilyStatus = familyStatus;
+            ResidencePlace = residencePlace;
+        }
+
+        public void AddSpecialNotes(string specialNote) { SpecialNotes += "\n\t"+specialNote; }
+        public void AddFamilyStatus(string familyStatus) { FamilyStatus += "\n\t" + familyStatus; }
+        public void AddResidencePlace(string residencePlace) { ResidencePlace += "\n\t" + residencePlace; }
+
+        public virtual void Print()
+        {
+            Console.WriteLine("Passport No:{0}", PassportNo);
+            Console.WriteLine("Surname: {0}",Surname);
+            Console.WriteLine("Name: {0}", Name);
+            Console.WriteLine("Patronymic name: {0}", PatronymicName);
+            Console.WriteLine("Date of birth: {0}", BirthDate.ToString("dd.MM.yyyy"));
+            Console.WriteLine("Place of birth: {0}", BirthPlace);
+            Console.WriteLine("Sex: {0}", Sex);
+            Console.WriteLine("Authority: {0}", Authority);
+            Console.WriteLine("Date of issue: {0}", IssueDate.ToString("dd.MM.yyyy"));
+            Console.WriteLine("Special notes: {0}", SpecialNotes);
+            Console.WriteLine("Family status: {0}", FamilyStatus);
+            Console.WriteLine("Residence place: {0}", ResidencePlace);
+        }
+    }
+}
