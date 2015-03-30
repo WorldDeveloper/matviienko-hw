5fa0badf459c92f34aa09d9e339a08c1ea055836
WorldDeveloper <matviienkos@gmail.com>
2015-03-31 00:36:03 +0300

    visual_cs exam

---
+++ visual_cs/exam/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace exam
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.TreeView1 = new System.Windows.Forms.TreeView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.copyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.icons = new System.Windows.Forms.ImageList(this.components);
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TreeView1
+            // 
+            this.TreeView1.CheckBoxes = true;
+            this.TreeView1.Location = new System.Drawing.Point(0, 27);
+            this.TreeView1.Name = "TreeView1";
+            this.TreeView1.Size = new System.Drawing.Size(850, 530);
+            this.TreeView1.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(850, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.copyToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(137, 22);
+            this.openToolStripMenuItem.Text = "Open folder";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // copyToolStripMenuItem
+            // 
+            this.copyToolStripMenuItem.Name = "copyToolStripMenuItem";
+            this.copyToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.copyToolStripMenuItem.Text = "Copy";
+            this.copyToolStripMenuItem.Click += new System.EventHandler(this.copyToolStripMenuItem_Click);
+            // 
+            // icons
+            // 
+            this.icons.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("icons.ImageStream")));
+            this.icons.TransparentColor = System.Drawing.Color.Transparent;
+            this.icons.Images.SetKeyName(0, "file.png");
+            this.icons.Images.SetKeyName(1, "folder.png");
+            this.icons.Images.SetKeyName(2, "opened_folder.png");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(850, 555);
+            this.Controls.Add(this.TreeView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "TreeView";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView TreeView1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem copyToolStripMenuItem;
+        private System.Windows.Forms.ImageList icons;
+    }
+}
+
---
+++ visual_cs/exam/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace exam
+{
+    public partial class Form1 : Form
+    {
+        private FolderBrowserDialog mFolderBrowser;
+        private string mPath;
+
+        public Form1()
+        {
+            InitializeComponent();
+            mFolderBrowser = new FolderBrowserDialog();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            DialogResult result = mFolderBrowser.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                mPath = mFolderBrowser.SelectedPath;
+                ListDirectory();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            mPath = @"\\fs\Преподаватели\Загоруйко А\Public";
+
+            if (Directory.Exists(mPath))
+            {
+                ListDirectory();
+            }
+            else
+            {
+                DialogResult result = mFolderBrowser.ShowDialog();
+                if (result == DialogResult.OK)
+                {
+                    mPath = mFolderBrowser.SelectedPath;
+                    ListDirectory();
+                }
+            }
+        }
+
+        private void ListDirectory()
+        {
+            TreeView1.ImageList = icons;
+            TreeView1.ImageIndex = 0;
+            TreeView1.SelectedImageIndex = 0;
+
+            TreeView1.Nodes.Clear();
+            var rootDirectoryInfo = new DirectoryInfo(mPath);
+            TreeView1.Nodes.Add(CreateDirectoryNode(rootDirectoryInfo));
+        }
+
+        private TreeNode CreateDirectoryNode(DirectoryInfo directoryInfo)
+        {
+            var directoryNode = new TreeNode(directoryInfo.Name);
+            directoryNode.ImageIndex = 1;
+            directoryNode.SelectedImageIndex = 2;
+
+            foreach (var directory in directoryInfo.GetDirectories())
+            {
+                if ((directory.Attributes & FileAttributes.Hidden) != FileAttributes.Hidden)
+                    directoryNode.Nodes.Add(CreateDirectoryNode(directory));
+            }
+
+            foreach (var file in directoryInfo.GetFiles())
+            {
+                directoryNode.Nodes.Add(new TreeNode(file.Name));
+            }
+
+            return directoryNode;
+        }
+
+        private void copyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            TreeNodeCollection nodes = TreeView1.Nodes;
+            foreach (TreeNode node in nodes)
+            {
+                PrintRecursive(node);
+            }
+        }
+
+        private void CopyFile(TreeNode node)
+        {
+            StringBuilder sb = new StringBuilder(node.FullPath.ToString());
+            sb = sb.Replace(TreeView1.Nodes[0].Text, "");
+            sb.Insert(0, mPath);
+
+            string fileName = node.Text;
+            string sourceFile = sb.ToString(); ;
+            string targetPath = mPath + "\\copy";
+            string destFile = System.IO.Path.Combine(targetPath, fileName);
+
+            if (File.Exists(sourceFile))
+            {
+                if (!System.IO.Directory.Exists(targetPath))
+                {
+                    System.IO.Directory.CreateDirectory(targetPath);
+                }
+
+                System.IO.File.Copy(sourceFile, destFile, true);
+            }
+        }
+
+        private void PrintRecursive(TreeNode treeNode)
+        {
+            if (treeNode.Checked == true) CopyFile(treeNode);
+            foreach (TreeNode tn in treeNode.Nodes)
+            {
+                PrintRecursive(tn);
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            TreeNodeCollection nodes = TreeView1.Nodes;
+            foreach (TreeNode node in nodes)
+            {
+                PrintRecursive(node);
+            }
+        }
+    }
+}
