6e8ee30122a3215238b367c8252edc8471eeeb73
WorldDeveloper <matviienkos@gmail.com>
2015-04-10 20:20:22 +0300

    gdi_1 (chess board) was added

---
+++ gdi/gdi_1/chess/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace chess
+{
+    class ChessBoard
+    {
+        public ChessBoard(Form parent, PaintEventArgs e)
+        {
+            int cellSize = 40;
+            int padding = 20; // min 10
+            int side = 8 * cellSize + padding * 2;
+            Size clientSize = new Size(side, side);
+            parent.ClientSize = clientSize;
+
+            Graphics g = e.Graphics;
+            Rectangle board = parent.ClientRectangle;
+            SolidBrush brush = new SolidBrush(Color.BurlyWood);
+            g.FillRectangle(brush, board);
+
+            Rectangle[,] cell = new Rectangle[8, 8];
+            Font font = new Font("Verdana", Math.Min(padding, cellSize) / 3, FontStyle.Bold);
+
+            for (int i = 0; i < 8; ++i)
+            {
+                Point numberPosition = new Point(i * cellSize + cellSize / 2 + padding, padding / 3);
+                g.DrawString(((char)('A' + i)).ToString(), font, Brushes.Black, numberPosition);
+            }
+            for (int i = 0; i < 8; ++i)
+            {
+                Point numberPosition = new Point(padding / 3, i * cellSize + cellSize / 2 + padding);
+                g.DrawString((8 - i).ToString(), font, Brushes.Black, numberPosition);
+                for (int j = 0; j < 8; ++j)
+                {
+                    cell[i, j] = new Rectangle(padding + i * cellSize, padding + j * cellSize, cellSize, cellSize);
+
+                    if ((i % 2 == 0 && j % 2 == 0) || (i % 2 == 1 && j % 2 == 1))
+                        g.FillRectangle(Brushes.White, cell[i, j]);
+                    else
+                        g.FillRectangle(brush, cell[i, j]);
+                }
+            }
+
+            int penSize = 2;
+            int frameTop = padding - penSize / 2;
+            Rectangle cellsFrame = new Rectangle(frameTop, frameTop, 8 * cellSize + penSize, 8 * cellSize + penSize);
+            Pen pen = new Pen(Brushes.White, penSize);
+            g.DrawRectangle(pen, cellsFrame);
+
+        }
+    }
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+             ChessBoard chessBoard = new ChessBoard(this, e);
+        }
+    }
+}
