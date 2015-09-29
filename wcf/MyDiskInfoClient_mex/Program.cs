f7166b400ac0471a4c858f627408ce843cf1d254
WorldDeveloper <matviienkos@gmail.com>
2015-09-29 23:30:21 +0300

    Added MyDiskInfoClient proxy

---
+++ wcf/MyDiskInfoClient_mex/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDiskInfoClient_mex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            localhost.MyDiskInfoClient proxy = new localhost.MyDiskInfoClient();
+
+            string diskName = "d";
+            Console.WriteLine("Disk {0}:\\", diskName.ToUpper());
+
+            Console.WriteLine("\n\tTotal size: {0}", proxy.TotalSpace(diskName).ToString());
+            Console.WriteLine("\n\tFree space: {0}", proxy.FreeSpace(diskName).ToString());
+
+            Console.WriteLine("\n\nPress Enter to exit");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ wcf/MyDiskInfoClient_mex/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MyDiskInfoClient_mex")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MyDiskInfoClient_mex")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("66e1531d-259c-40e8-9eba-d71c878b286e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
