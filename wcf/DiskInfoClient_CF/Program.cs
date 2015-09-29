dbf121d7d6e5d0e96fcc0694e6185b7cc9a7e218
WorldDeveloper <matviienkos@gmail.com>
2015-09-29 21:49:10 +0300

    WCF homework - channel factory

---
+++ wcf/DiskInfoClient_CF/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiskInfoClient_CF
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            ChannelFactory<IMyDiskInfo> factory = new ChannelFactory<IMyDiskInfo>(new BasicHttpBinding(), new EndpointAddress("http://localhost:8080/MyDiskInfo/ep1"));
+            IMyDiskInfo channel = factory.CreateChannel();
+
+            string diskName = "c";
+            Console.WriteLine("Disk {0}:\\", diskName.ToUpper());
+
+            Console.WriteLine("\n\tTotal size: {0}", channel.TotalSpace(diskName).ToString());
+            Console.WriteLine("\n\tFree space: {0}", channel.FreeSpace(diskName).ToString());
+
+            Console.WriteLine("\n\nPress Enter to exit");
+            Console.ReadLine();
+        }
+
+        [ServiceContract]
+        public interface IMyDiskInfo
+        {
+            [OperationContract]
+            string FreeSpace(string disk);
+            [OperationContract]
+            string TotalSpace(string disk);
+        }
+    }
+}
---
+++ wcf/DiskInfoServer_CF/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiskInfoServer_CF
+{
+
+    [ServiceContract]
+    public interface IMyDiskInfo
+    {
+        [OperationContract]
+        string FreeSpace(string disk);
+        [OperationContract]
+        string TotalSpace(string disk);
+    }
+
+    public class MyDiskInfo : IMyDiskInfo
+    {
+        public string FreeSpace(string disk)
+        {
+            if (string.IsNullOrWhiteSpace(disk))
+                return "Wrong disk!";
+
+            disk = disk.Substring(0, 1).ToUpper()+":\\";
+            DriveInfo[] drives = DriveInfo.GetDrives();
+            foreach (DriveInfo drive in drives)
+            {
+                if (drive.IsReady == true && drive.Name == disk)
+                   return drive.TotalFreeSpace.ToString();
+            }
+
+            return "Wrong disk!";
+        }
+
+        public string TotalSpace(string disk)
+        {
+            if (string.IsNullOrWhiteSpace(disk))
+                return "Wrong disk!";
+
+            disk = disk.Substring(0, 1).ToUpper() + ":\\";
+            DriveInfo[] drives = DriveInfo.GetDrives();
+            foreach (DriveInfo drive in drives)
+            {
+                if (drive.IsReady == true && drive.Name == disk)
+                    return drive.TotalSize.ToString();
+            }
+
+            return "Wrong disk!";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost serviceHost = new ServiceHost(typeof(MyDiskInfo));
+            serviceHost.AddServiceEndpoint(typeof(IMyDiskInfo), new BasicHttpBinding(), "http://localhost:8080/MyDiskInfo/ep1");
+            serviceHost.Open();
+
+            Console.WriteLine("Press Enter to exit");
+            Console.ReadLine();
+            serviceHost.Close();
+        }
+    }
+}
